c803f529e7f37c0979751b50606b9053ee8898ae
onsen koichi <mstdn.nagasaki@gmail.com>
2024-04-15 23:32:39 +0900

    Add files via upload

---
+++ Exp/LiteDbToLcsv.cs
@@ -0,0 +1,115 @@
+﻿using LiteDB;
+
+using System.Text;
+
+namespace LiteDB.Export
+{
+    internal class LiteDbToLcsv
+    {
+        string _liteDbPath = "";
+        public LiteDbToLcsv(string liteDbPath)
+        {
+            _liteDbPath = liteDbPath;
+        }
+
+        public bool Execute(string lcsvDir)
+        {
+            if (string.IsNullOrEmpty(_liteDbPath) || string.IsNullOrEmpty(lcsvDir)) return false;
+
+            if (!Directory.Exists(lcsvDir)) return false;
+
+            try
+            {
+                var buff = new StringBuilder();
+
+                using (var context = new LiteDatabase(_liteDbPath))
+                {
+
+                    var collections = context.GetCollectionNames();
+                    if (collections == null) return false;
+
+                    foreach (var collection in collections)
+                    {
+                        var target = context.GetCollection(collection);
+                        if (target == null) continue;
+
+                        var records = target.FindAll();
+
+                        buff.AppendLine(collection);
+
+                        var isFirst = false;
+                        foreach (var record in records)
+                        {
+                            if (record.IsArray) throw new NotSupportedException();
+
+                            if (!isFirst)
+                            {
+
+                                // Names
+                                buff.AppendLine(string.Join(",", record.Keys.ToArray()));
+
+                                // Types
+                                buff.AppendLine(string.Join(",", ConvertString(record.Values.ToArray())));
+
+                                isFirst = true;
+                            }
+
+                            buff.AppendLine(string.Join(",", record.Values));
+
+
+                        }
+
+                        using (var writer = new StreamWriter(lcsvDir + "\\" + collection + ".lcsv"))
+                        {
+                            writer.WriteLine(buff.ToString());
+                        }
+
+                    }
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        private string[] ConvertString(BsonValue[] values)
+        {
+            var result = new string[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                switch (values[i].Type)
+                {
+                    case BsonType.ObjectId:
+                        result[i] = "objectid";
+                        break;
+                    case BsonType.String:
+                        result[i] = "string";
+                        break;
+                    case BsonType.DateTime:
+                        result[i] = "datetime";
+                        break;
+                    case BsonType.Int32:
+                        result[i] = "int";
+                        break;
+                    case BsonType.Double:
+                        result[i] = "double";
+                        break;
+                    case BsonType.Int64:
+                        result[i] = "long";
+                        break;
+                    case BsonType.Boolean:
+                        result[i] = "long";
+                        break;
+                    default:
+                        throw new NotSupportedException();
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Exp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LiteDB.Export
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                if (!File.Exists(args[0]))
+                {
+                    Console.WriteLine("file not found." + args[0]);
+                    return;
+                }
+
+                var exportor = new LiteDbToLcsv(args[0]);
+                if (exportor.Execute(args[1]))
+                {
+                    Console.WriteLine("Completed!");
+                }
+                else
+                {
+                    Console.WriteLine("Failed!");
+                }
+                return;
+            }
+
+            Console.WriteLine("invalid argument.");
+            Console.WriteLine("");
+            Console.WriteLine(" > LiteDb.Export.exe [LiteDB.db] [output lcsv-file dir.]");
+        }
+    }
+}
